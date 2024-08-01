3995b10c40399254bc21a738a1ac6d69f8702ea6
Ari <ari.rafiq.ph@gmail.com>
2024-08-01 11:21:26 +0300

    Add files via upload

---
+++ mainscript.js
@@ -0,0 +1,9 @@
+document.getElementById('vitaminD3').addEventListener('click', function() {
+    alert('Coming Soon...');
+    location.href = 'https://kurdph.com';
+});
+
+document.getElementById('comingsoon').addEventListener('click', function() {
+    alert('Coming Soon...');
+    location.href = 'https://kurdph.com';
+});
