ab6cc4a63c03d543fe96792fda081fc7eefb5ffd
Ari <ari.rafiq.ph@gmail.com>
2024-08-03 11:32:40 +0300

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,9 +1,1 @@
-document.getElementById('vitaminD3').addEventListener('click', function() {
-    alert('Coming Soon...');
-    location.href = 'https://kurdph.com';
-});
 
-document.getElementById('comingsoon').addEventListener('click', function() {
-    alert('Coming Soon...');
-    location.href = 'https://kurdph.com';
-});
