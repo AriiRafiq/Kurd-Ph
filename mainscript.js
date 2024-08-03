f2ec64f7506ae538bf470b8010918371d4084479
Ari <ari.rafiq.ph@gmail.com>
2024-08-03 23:04:32 +0300

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,4 +1,3 @@
 document.getElementById('comingsoon').addEventListener('click', function() {
-    alert('Coming Soon...');
-    location.href = 'https://kurdph.com';
+    alert('Stay tuned for more updates!');
 });
